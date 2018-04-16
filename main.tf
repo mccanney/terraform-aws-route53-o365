6e93ad6847b73915a4e90881404aad7ac1c97004
David McCanney <david@mccanney.org>
2018-04-16 15:27:16 +0100

    Interesting experimentation.

--- main.tf
+++ main.tf
@@ -10,6 +10,18 @@ data "template_file" "mx_record" {
     template = "10 ${replace("${var.domain}", ".", "-")}.mail.protection.outlook.com"
 }
 
+data "template_file" "passed_zoneid" {
+    count = "${length("${var.depends_on}")}"
+    
+    template = "${var.depends_on}"
+}
+
+data "template_file" "computed_zoneid" {
+    count = "${1 - length("${var.depends_on}")}"
+
+    template = "${data.template_file.mx_record.rendered}"
+}
+
 #################
 # Exchange Online
 #################
@@ -17,97 +29,9 @@ data "template_file" "mx_record" {
 resource "aws_route53_record" "mx" {
     count   = "${var.enable_exchange ? 1 : 0}"
 
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
+    zone_id = "${element(concat(data.template_file.passed_zoneid.*.rendered, data.template_file.computed_zoneid.*.rendered), 0)}"   
     name    = ""
     records = ["${data.template_file.mx_record.rendered}"]
     type    = "MX"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "autodiscover" {
-    count   = "${var.enable_exchange ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "autodiscover"
-    records = ["autodiscover.outlook.com"]
-    type    = "CNAME"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "spf" {
-    count   = "${var.enable_exchange ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = ""
-    records = ["MS=${var.ms_txt}","v=spf1 include:spf.protection.outlook.com -all"]
-    type    = "TXT"
-    ttl     = "${var.ttl}"
-}
-
-####################
-# Skype for Business
-####################
-
-resource "aws_route53_record" "lyncdiscover" {
-    count   = "${var.enable_sfb ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "lyncdiscover"
-    records = ["webdir.online.lync.com"]
-    type    = "CNAME"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "sip" {
-    count   = "${var.enable_sfb ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "sip"
-    records = ["sipdir.online.lync.com"]
-    type    = "CNAME"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "sipdir" {
-    count   = "${var.enable_sfb ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "_sipfederationtls._tcp"
-    records = ["100 1 5061 sipfed.online.lync.com"]
-    type    = "SRV"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "sipfed" {
-    count   = "${var.enable_sfb ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "_sip._tls"
-    records = ["100 1 443 sipdir.online.lync.com"]
-    type    = "SRV"
-    ttl     = "${var.ttl}"
-}
-
-##########################
-# Mobile Device Management
-##########################
-
-resource "aws_route53_record" "enterpriseregistration" {
-    count   = "${var.enable_mdm ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "enterpriseregistration"
-    records = ["enterpriseregistration.windows.net"]
-    type    = "CNAME"
-    ttl     = "${var.ttl}"
-}
-
-resource "aws_route53_record" "enterpriseenrollment" {
-    count   = "${var.enable_mdm ? 1 : 0}"
-
-    zone_id = "${data.aws_route53_zone.selected.zone_id}"
-    name    = "enterpriseenrollment"
-    records = ["enterpriseenrollment.manage.microsoft.com"]
-    type    = "CNAME"
     ttl     = "${var.ttl}"
 }
