a2a4494552fbe4174aa5cea4be3a835bae69211b
David McCanney <david@mccanney.org>
2018-04-16 21:34:22 +0100

    Expanding testing.

--- test/integration/main.tf
+++ test/integration/main.tf
@@ -1,17 +1,18 @@
 provider "aws" {
     version = "~> 1.14"
-    region  = "us-east-1"
+    region  = "${var.region}"
 }
 
 resource "aws_route53_zone" "test_zone" {
-    name              = "tiguard.technology"
+    name              = "${var.domain}"
     comment           = "Route53 DNS zone for tiguard.technology"
+    delegation_set_id = "${var.delegation_id}"
 }
 
-module "terraform-aws-office365" {
+module "route53-o365" {
     source = "../.."
 
-    domain          = "tiguard.technology"
+    domain          = "${var.domain}"
     zone_id         = "${aws_route53_zone.test_zone.zone_id}"
     ms_txt          = "ms12345678"
     enable_exchange = true
---
+++ test/integration/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "domain" {
+    default = "tiguard.technology"
+}
+
+variable "delegation_id" {
+    default = ""
+}
