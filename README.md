d83461cadf81a57e5c7be57dfb5cd8e036b4739e
David McCanney <david@mccanney.org>
2018-04-16 17:08:45 +0100

    Expanded README.

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# terraform-office365
+# AWS Office 365 Route53 Terraform Module
 
 [![CircleCI](https://circleci.com/gh/tiguard/terraform-aws-office365.svg?style=shield)](https://circleci.com/gh/tiguard/terraform-aws-office365)
+[![GitHub release](https://img.shields.io/github/release/tiguard/terraform-aws-office365.svg?style=flat-square)](https://github.com/tiguard/terraform-aws-office365)
+[![license](https://img.shields.io/github/license/tiguard/terraform-aws-office365.svg?style=flat-square)](https://github.com/tiguard/terraform-aws-office365/blob/master/LICENSE.md)
+
+A terraform module which creates, in AWS Route53, the [DNS records](https://support.office.com/article/External-Domain-Name-System-records-for-Office-365-c0531a6f-9e25-4f2d-ad0e-a70bfef09ac0) required by Office 365.
+
+## Usage
+
+```hcl
+module "route53_o365" {
+    source = "tiguard/route53-o365/aws"
+
+    domain          = "example.com"
+    zone_id         = "${aws_route53_zone.zone_name.zone_id}"
+    ms_txt          = "ms12345678"
+    enable_exchange = true
+    enable_sfb      = true
+    enable_mdm      = true
+}
+```
+
+## Examples
+
+* [Usage with new zone](examples/new-zone/README.md)
+* [Usage with existing zone](examples/existing-zone/README.md)
+
+## Contributing
+
+Please use the [issue tracker](https://github.com/tiguard/terraform-aws-route53-o365/issues) to file any bug reports or make feature requests.
+
+## License
+
+Released under the [MIT license](LICENSE.md).
--- variables.tf
+++ variables.tf
@@ -1,34 +1,34 @@
 variable "domain" {
-    description = "The name of the domain to create Office365 DNS records in"
+    description = "The DNS name of the domain in which to create the Office 365 records."
     default     = ""
 }
 
 variable "zone_id" { 
-    description = "The Zone ID of the Route53 DNS zone"
+    description = "The Zone ID of the Route53 DNS zone."
     default     = ""
 }
 
 variable "ttl" {
-    description = "The time-to-live value for the DNS record"
+    description = "The time-to-live value for the DNS records."
     default     = "3600"
 }
 
 variable "ms_txt" {
-    description = "The value of the MS=mxXYZ TXT record Office365 requires to prove domain ownership"
+    description = "The value of the MS=mxXYZ TXT record which Office 365 requires to prove domain ownership."
     default     = ""
 }
 
 variable "enable_exchange" {
-    description = "Controls if the DNS records for Exchange Online should be created"
+    description = "Controls if the DNS records for Exchange Online should be created."
     default     = false
 }
 
 variable "enable_sfb" {
-    description = "Controls if the DNS records for Skype for Business should be created"
+    description = "Controls if the DNS records for Skype for Business should be created."
     default     = false
 }
 
 variable "enable_mdm" {
-    description = "Controls if the DNS records for Mobile Device Management should be created"
+    description = "Controls if the DNS records for Mobile Device Management should be created."
     default     = false
 }
